38a4d15f570c56fd3201fcd1044ea29f8de78d1b
Cem Aksoylar <caksoylar@gmail.com>
2015-09-30 17:47:34 -0400

    random projection for hyperplane rounding and larger example in script

--- constr.m
+++ constr.m
@@ -2,20 +2,31 @@ clear all;
 close all;
 % cvx_solver sedumi
 
-%% data generation, generate n1xn2 lattice, 4-connected
+%% data generation, generate n1xn2 lattice, 4-connected, small example
 n1 = 5;
 n2 = 5;
 n = n1*n2;
 K = 4;
 xx_line = zeros(n,1);
-% xx_line([7, 8, 9, 12, 13, 14, 19, 20, 24]) = 1;
-xx_line([7, 9, 10, 15]) = 1;  % 4 nodes with value 1
-% xx_line(9) = 1.1;
+xx_line([7, 9, 10, 15]) = 1;  % 4 nodes with value 1, 1 node disconnected
 xx = reshape(xx_line, n1, n2);
 
 % observations with no noise
-yy = xx;
-yy_line = xx_line; %0.01*randn(size(xx_line));
+yy = xx; % + 0.01*randn(size(xx));
+
+%% data generation, generate n1xn2 lattice, 4-connected, larger example
+% (slow and takes a lot of memory)
+n1 = 8;
+n2 = 8;
+n = n1*n2;
+K = 9;
+
+xx = zeros(n1, n2);
+xx(3:4, 2:4) = 1;  % larger connected component
+xx(5, 6:8) = 1;  % smaller connected component
+
+% observations with some noise
+yy = xx + 0.01*randn(size(xx));
 
 %% compute adjacency matrix, Laplacian matrix
 % unweighted adjacency matrix
@@ -32,21 +43,10 @@ for i=1:n1
         end
     end
 end
-% gplot(Adj, xxg, '-o');
-% axis([0, n2+1, 0, n1+1]);
 % unnormalized graph Laplacian
 d = sum(Adj,2);
 D = diag(d);
 L = D-Adj;
-
-% plot using gplot, not used for now
-% [xg, yg] = meshgrid(1:n1, 1:n2);
-% xxg = [xg(:), yg(:)];
-
-% figure, gplot(Adj, xxg, ':x');
-% hold on;
-% gplot(diag(xx_line)*Adj*diag(xx_line), xxg, 'r-o');
-% axis([0, n2+1, 0, n1+1]);
 
 % plot using imagesc to display as matrix
 figure, imagesc(xx), title('Original');
@@ -60,35 +60,48 @@ cvx_begin
     expression L_M(n,n)  % laplacian of M
     expression A_M(n,n)  % A.M
     expression L_A_M(n,n)  % laplacian of A.M
-    
+
     L_M = diag(sum(M,2)) - M;
     A_M = Adj.*M;
     L_A_M = diag(sum(A_M,2)) - A_M;
-    
+
     maximize( trace( diag(yy(:)) .* M ) )
     subject to
         M == semidefinite(n)
         M >= 0
-        trace( M ) <= K
-%         sum(sum(M)) <= K
+        trace( M ) <= 1
+        sum(sum(M)) <= K
         L_A_M - gamma*L_M == semidefinite(n)
 
+        % constraint so that not all is concentrated on one diagonal elt if diags of yy are
+        % not exactly equal (e.g. noisy y), forces the solution to have exactly K non-zero diags
+        diag(M) <= 1/K
+
         % constaints from NIPS paper
-        M <= 1
-        M(p,p) == 1
-        for i = 1:n
-            M(i,i) <= M(p,i)
-            M(i,:) <= M(i,i)
-        end
+%         M <= 1
+%         M(p,p) == 1
+%         for i = 1:n
+%             M(i,i) <= M(p,i)
+%             M(i,:) <= M(i,i)
+%         end
 cvx_end
 
-S = diag(M);% > 1e-3;
+% show result using imagesc, no thresholding
+figure, imagesc(reshape(diag(M), n1, n2)), colorbar, title('Estimated');
 
-% show result using gplot, not used for now
-% figure, gplot(Adj, xxg, ':x');
-% hold on;
-% gplot(diag(S)*Adj*diag(S), xxg, 'r-o');
-% axis([0, n2+1, 0, n1+1]);
+%% project and display
 
-% show result using imagesc, no thresholding for now
-figure, imagesc(reshape(S, n1, n2)), colorbar, title('Estimated');
+S = diag(M) > 1e-3;
+Msub = M(find(S), find(S));  % non-zero submatrix of M
+
+% lattice coordinates of indices in M
+[ind1, ind2] = ind2sub([n1 n2], (1:n)');
+ind_text = strcat(num2str(ind1), ',', num2str(ind2));
+ind_sub = ind_text(S, :);
+
+% random projection to 3-dim
+% R = (rand(size(Msub,1),3) > 0.5)*2 - 1;  % random binary vectors
+R = randn(size(Msub,1),3);  % random Gaussian vectors
+V = Msub*R;
+figure, scatter3(V(:,1), V(:,2), V(:,3)), xlim([-1 1]), ylim([-1 1]), hold on
+text(V(:,1), V(:,2)+0.05, V(:,3), ind_sub), title('Random projection')
